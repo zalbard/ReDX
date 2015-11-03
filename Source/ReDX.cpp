a922637356b7871fc203d81e896d98f076fb98fa
Evgenii Golubev <zalbard@gmail.com>
2015-11-03 15:20:35 +0100

    DirectXMath hardware support verification added.

--- Source/ReDX.cpp
+++ Source/ReDX.cpp
@@ -1,12 +1,21 @@
 #include <cstdio>
 #include <d3d12.h>
+#include <Windows.h>
+#include <DirectXMath.h>
 
 int main(const int argc, const char* argv[]) {
+    // Parse command line arguments
 	if (argc > 1) {
-		printf("The following command line arguments have been ignored:\n");
+		fprintf(stderr, "The following command line arguments have been ignored:\n");
         for (auto i = 1; i < argc; ++i) {
-            printf("%s\n", argv[i]);
+            fprintf(stderr, "%s ", argv[i]);
         }
+        fputs("\n", stderr);
 	}
+    // Verify SSE2 support for DirectXMath library
+    if (!DirectX::XMVerifyCPUSupport()) {
+        fprintf(stderr, "The CPU doesn't support SSE2. Aborting.\n");
+        return -1;
+    }
 	return 0;
 }
