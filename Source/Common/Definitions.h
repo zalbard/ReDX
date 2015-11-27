f30e98ab74a1a930620150120459f40662a0e308
Evgenii Golubev <zalbard@gmail.com>
2015-11-27 10:29:52 +0100

    Use "Otor" instead of "Op" as a shorthand for "Operator".

--- Source/Common/Definitions.h
+++ Source/Common/Definitions.h
@@ -7,14 +7,14 @@
                         T& operator=(T&&) noexcept = default; \
                         ~T()                       = default
 
-// Declares an -explicit- Dtor, copy/move Ctors and assignment Ops
+// Declares an -explicit- Dtor, copy/move Ctors and assignment Otors
 #define RULE_OF_FIVE(T) T(const T&);                \
                         T& operator=(const T&);     \
                         T(T&&) noexcept;            \
                         T& operator=(T&&) noexcept; \
                         ~T()
 
-// Declares a -default- Dtor, a move Ctor and an assignment Op
+// Declares a -default- Dtor, a move Ctor and an assignment Otor
 // Forbids copy construction and copy assignment
 #define RULE_OF_ZERO_MOVE_ONLY(T) T(const T&)                = delete;  \
                                   T& operator=(const T&)     = delete;  \
@@ -22,7 +22,7 @@
                                   T& operator=(T&&) noexcept = default; \
                                   ~T()                       = default
 
-// Declares an -explicit- Dtor, a move Ctor and an assignment Op
+// Declares an -explicit- Dtor, a move Ctor and an assignment Otor
 // Forbids copy construction and copy assignment
 #define RULE_OF_FIVE_MOVE_ONLY(T) T(const T&)            = delete; \
                                   T& operator=(const T&) = delete; \
