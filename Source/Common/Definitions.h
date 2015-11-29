8e05d089f7b89a1c7ec1d0257e99cd56ed27902b
zalbard <zalbard@gmail.com>
2015-11-29 01:22:32 +0100

    Declared destructors as noexcept
    Destructors are now explicitly declared as noexcept which mirrors the (implicit) C++ 11 defaults.

--- Source/Common/Definitions.h
+++ Source/Common/Definitions.h
@@ -5,14 +5,14 @@
                         T& operator=(const T&)     = default; \
                         T(T&&) noexcept            = default; \
                         T& operator=(T&&) noexcept = default; \
-                        ~T()                       = default
+                        ~T() noexcept              = default
 
 // Declares an -explicit- Dtor, copy/move Ctors and assignment Otors
 #define RULE_OF_FIVE(T) T(const T&);                \
                         T& operator=(const T&);     \
                         T(T&&) noexcept;            \
                         T& operator=(T&&) noexcept; \
-                        ~T()
+                        ~T() noexcept
 
 // Declares a -default- Dtor, a move Ctor and an assignment Otor
 // Forbids copy construction and copy assignment
@@ -20,7 +20,7 @@
                                   T& operator=(const T&)     = delete;  \
                                   T(T&&) noexcept            = default; \
                                   T& operator=(T&&) noexcept = default; \
-                                  ~T()                       = default
+                                  ~T() noexcept              = default
 
 // Declares an -explicit- Dtor, a move Ctor and an assignment Otor
 // Forbids copy construction and copy assignment
@@ -28,7 +28,7 @@
                                   T& operator=(const T&) = delete; \
                                   T(T&&) noexcept;                 \
                                   T& operator=(T&&) noexcept;      \
-                                  ~T()
+                                  ~T() noexcept
 
 // Declares a singleton class which cannot be
 // constructed, destroyed, copied, moved or assigned
@@ -37,7 +37,7 @@
                      T& operator=(const T&)     = delete; \
                      T(T&&) noexcept            = delete; \
                      T& operator=(T&&) noexcept = delete; \
-                     ~T()                       = delete
+                     ~T() noexcept              = delete
 
 using uint   = unsigned int;
 using uint8  = unsigned short;
