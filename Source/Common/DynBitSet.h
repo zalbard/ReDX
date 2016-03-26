7e56547c8ea0981e57718c28d50c2aca78ab7e3b
Evgenii Golubev <zalbard@gmail.com>
2016-03-26 10:12:12 +0100

    Reformat DynBitSet class

--- Source/Common/DynBitSet.h
+++ Source/Common/DynBitSet.h
@@ -3,23 +3,23 @@
 #include <memory>
 #include "Definitions.h"
 
-// BitSet with size specified at runtime
+// BitSet with size specified at runtime.
 class DynBitSet {
 public:
     RULE_OF_FIVE(DynBitSet);
-    // Ctor; performs zero-initialization
+    // Ctor; performs zero-initialization.
     DynBitSet();
-    // Ctor; takes the size (number of bits) as input
+    // Ctor; takes the size (number of bits) as input.
     explicit DynBitSet(const uint size);
-    // Resets the values of all bits to 'value' (0 or 1)
+    // Resets the values of all bits to 'value' (0 or 1).
     void reset(const bool value);
-    // Sets the value of the specified bit to 0
+    // Sets the value of the specified bit to 0.
     void clearBit(const uint index);
-    // Sets the value of the specified bit to 1
+    // Sets the value of the specified bit to 1.
     void setBit(const uint index);
-    // Inverts the value of the specified bit
+    // Inverts the value of the specified bit.
     void toggleBit(const uint index);
-    // Returns 'true' if the specified bit is 1, 'false' otherwise
+    // Returns 'true' if the specified bit is 1, 'false' otherwise.
     bool testBit(const uint index) const;
 private:
     std::unique_ptr<uint[]> m_bits;
