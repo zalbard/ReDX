ffb3690cdcabce61d537339628f9518e1275bfbd
Evgenii Golubev <zalbard@gmail.com>
2016-03-26 10:07:42 +0100

    Reformat Constants.h

--- Source/Common/Constants.h
+++ Source/Common/Constants.h
@@ -3,7 +3,7 @@
 #include <dxgiformat.h>
 #include "Definitions.h"
 
-// Mathematical constants
+// Mathematical constants.
 constexpr float M_E             = 2.71828175f;   // e
 constexpr float M_LOG2E         = 1.44269502f;   // log2(e)
 constexpr float M_LOG10E        = 0.434294492f;  // log10(e)
@@ -21,23 +21,23 @@ constexpr float M_SQRT1_2       = 0.707106769f;  // 1/sqrt(2)
 constexpr long  WND_WIDTH       = 1280;
 // Vertical window size (in pixels). Not the same as the rendering resolution!
 constexpr long  WND_HEIGHT      = 720;
-// Vertical field of view (in radians)
+// Vertical field of view (in radians).
 constexpr float VERTICAL_FOV    = M_PI / 3.f;
-// Double/triple buffering
+// Double/triple buffering.
 constexpr uint  BUF_CNT         = 3;
-// Maximal rendering queue depth (determines the frame latency)
+// Maximal rendering queue depth (determines the frame latency).
 constexpr uint  FRAME_CNT       = 2;
-// Vertical blank count after which the synchronization is performed 
+// Vertical blank count after which the synchronization is performed. 
 constexpr uint  VSYNC_INTERVAL  = 0;
-// Software rendering flag
+// Software rendering flag.
 constexpr bool  USE_WARP_DEVICE = false;
-// Render target view format
+// Render target view format.
 constexpr auto  RTV_FORMAT      = DXGI_FORMAT_R8G8B8A8_UNORM;
-// Depth stencil view format
+// Depth stencil view format.
 constexpr auto  DSV_FORMAT      = DXGI_FORMAT_D24_UNORM_S8_UINT;
-// Upload buffer size (8 MiB)
+// Upload buffer size (8 MiB).
 constexpr uint  UPLOAD_BUF_SIZE = 8 * 1024 * 1024;
-// Camera's speed (in meters/sec)
+// Camera's speed (in meters/sec).
 constexpr float CAM_SPEED       = 500.f;
-// Camera's angular speed (in radians/sec)
+// Camera's angular speed (in radians/sec).
 constexpr float CAM_ANG_SPEED   = 2.0f;
