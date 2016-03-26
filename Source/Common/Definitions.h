98fb6659224a241989945864e85b2d3df54b2ef4
Evgenii Golubev <zalbard@gmail.com>
2016-03-26 10:09:53 +0100

    Reformat Definitions.h

--- Source/Common/Definitions.h
+++ Source/Common/Definitions.h
@@ -1,29 +1,29 @@
 #pragma once
 
-// Declares a -default- Dtor, copy/move Ctors and assignment Ops
+// Declares -default- Dtor, copy/move Ctors and assignment Otors.
 #define RULE_OF_ZERO(T) T(const T&)                = default; \
                         T& operator=(const T&)     = default; \
                         T(T&&) noexcept            = default; \
                         T& operator=(T&&) noexcept = default; \
                         ~T() noexcept              = default
 
-// Declares an -explicit- Dtor, copy/move Ctors and assignment Otors
+// Declares -explicit- Dtor, copy/move Ctors and assignment Otors.
 #define RULE_OF_FIVE(T) T(const T&);                \
                         T& operator=(const T&);     \
                         T(T&&) noexcept;            \
                         T& operator=(T&&) noexcept; \
                         ~T() noexcept
 
-// Declares a -default- Dtor, a move Ctor and an assignment Otor
-// Forbids copy construction and copy assignment
+// Declares -default- Dtor, a move Ctor and an assignment Otor.
+// Forbids copy construction and copy assignment.
 #define RULE_OF_ZERO_MOVE_ONLY(T) T(const T&)                = delete;  \
                                   T& operator=(const T&)     = delete;  \
                                   T(T&&) noexcept            = default; \
                                   T& operator=(T&&) noexcept = default; \
                                   ~T() noexcept              = default
 
-// Declares an -explicit- Dtor, a move Ctor and an assignment Otor
-// Forbids copy construction and copy assignment
+// Declares -explicit- Dtor, a move Ctor and an assignment Otor.
+// Forbids copy construction and copy assignment.
 #define RULE_OF_FIVE_MOVE_ONLY(T) T(const T&)            = delete; \
                                   T& operator=(const T&) = delete; \
                                   T(T&&) noexcept;                 \
@@ -31,7 +31,7 @@
                                   ~T() noexcept
 
 // Declares a singleton class which cannot be
-// constructed, destroyed, copied, moved or assigned
+// constructed, destroyed, copied, moved or assigned.
 #define SINGLETON(T) T()                        = delete; \
                      T(const T&)                = delete; \
                      T& operator=(const T&)     = delete; \
@@ -39,14 +39,14 @@
                      T& operator=(T&&) noexcept = delete; \
                      ~T() noexcept              = delete
 
-// Typedefs
+// Typedefs.
 using byte   = unsigned char;
 using int64  = long long int;
 using uint   = unsigned int;
 using uint16 = unsigned short;
 using uint64 = unsigned long long int;
 
-// Windows header macro definition removal
+// Remove Windows header macro definitions.
 #ifdef min
 #undef min
 #endif
