6a3d5dc76ae208ba128f556924422b20a9a1cad8
Evgenii Golubev <zalbard@gmail.com>
2016-04-05 16:40:48 +0200

    Implement align() for integral types

--- Source/Common/Utility.h
+++ Source/Common/Utility.h
@@ -8,12 +8,22 @@
 
 // Aligns the memory address to the next multiple of alignment.
 template <uint64 alignment>
-static inline auto align(const void* address)
+static inline auto align(void* address)
 -> byte* {
     // Make sure that the alignment is non-zero, and is a power of 2.
     static_assert((0 != alignment) && (0 == (alignment & (alignment - 1))), "Invalid alignment.");
-    const uint64 addr = reinterpret_cast<uint64>(address);
-    return reinterpret_cast<byte*>((addr + (alignment - 1)) & ~(alignment - 1));
+    const uint64 numVal = reinterpret_cast<uint64>(address);
+    return reinterpret_cast<byte*>((numVal + (alignment - 1)) & ~(alignment - 1));
+}
+
+// Aligns the integral value to the next multiple of alignment.
+template <uint64 alignment>
+static inline auto align(const uint value)
+-> uint {
+    // Make sure that the alignment is non-zero, and is a power of 2.
+    static_assert((0 != alignment) && (0 == (alignment & (alignment - 1))), "Invalid alignment.");
+    const uint64 numVal = static_cast<uint64>(value);
+    return static_cast<uint>((numVal + (alignment - 1)) & ~(alignment - 1));
 }
 
 // For internal use only!
