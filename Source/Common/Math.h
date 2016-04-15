ed19c30d23067ee2a09e4ac02883b35353a87af2
Evgenii Golubev <zalbard@gmail.com>
2016-04-15 15:13:44 +0200

    Move math functions to 'Math.h'

--- Source/Common/Math.h
+++ Source/Common/Math.h
@@ -16,6 +16,45 @@ static inline auto rcp(const T v)
     return 1.f / v;
 }
 
+// Returns 'true' if 'v' is a power of 2.
+template <typename T>
+constexpr auto isPow2(const T v)
+-> bool {
+    return 0 == (v & (v - 1));
+}
+
+// Aligns the memory address to the next multiple of alignment.
+template <uint64 alignment>
+static inline auto align(void* address)
+-> byte* {
+    // Make sure that the alignment is non-zero, and is a power of 2.
+    static_assert((alignment != 0) && isPow2(alignment), "Invalid alignment.");
+    const uint64 numVal = reinterpret_cast<uint64>(address);
+    return reinterpret_cast<byte*>((numVal + (alignment - 1)) & ~(alignment - 1));
+}
+
+// Aligns the integral value to the next multiple of alignment.
+template <uint64 alignment>
+static inline auto align(const uint value)
+-> uint {
+    // Make sure that the alignment is non-zero, and is a power of 2.
+    static_assert((alignment != 0) && isPow2(alignment), "Invalid alignment.");
+    const uint64 numVal = static_cast<uint64>(value);
+    return static_cast<uint>((numVal + (alignment - 1)) & ~(alignment - 1));
+}
+
+// Computes the integer value of log2 of 'v'.
+static inline auto log2u(const uint v)
+-> uint {
+    assert(isPow2(v));
+    static const uint b[] = {0xAAAAAAAA, 0xCCCCCCCC, 0xF0F0F0F0, 0xFF00FF00, 0xFFFF0000};
+    uint r = (v & b[0]) != 0;
+    for (uint i = 4; i > 0; --i) {
+        r |= ((v & b[i]) != 0) << i;
+    }
+    return r;
+}
+
 namespace DirectX {
     // Constructs an infinite reversed projection matrix.
     // The distance to the near plane is infinite, the distance to the far plane is 1.
