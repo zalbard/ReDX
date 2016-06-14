cacdebe3bd2cb4bd3751cf7be124a73de85083c9
Evgenii Golubev <zalbard@gmail.com>
2016-06-14 16:54:42 +0200

    Add AABox class
    And use XMFLOAT4A for Sphere storage.

--- Source/Common/Primitives.cpp
+++ Source/Common/Primitives.cpp
@@ -6,12 +6,22 @@ Sphere::Sphere(const XMFLOAT3& center, const float radius)
     : m_data{center.x, center.y, center.z, radius} {}
 
 XMVECTOR Sphere::center() const {
-    return m_data;
+	return SSE4::XMVectorSetW(XMLoadFloat4A(&m_data), 1.f);
 }
 
 XMVECTOR Sphere::radius() const {
-    return XMVectorSplatW(m_data);
+    return XMVectorSplatW(XMLoadFloat4A(&m_data));
 }
+
+AABox::AABox(const XMFLOAT3& pMin, const XMFLOAT3& pMax)
+	: m_pMin{pMin}
+	, m_pMax{pMax} {}
+
+AABox::AABox(const XMFLOAT3& pMin, const float (&dims)[3])
+	: m_pMin{pMin}
+	, m_pMax{pMin.x + dims[0],
+			 pMin.y + dims[1],
+			 pMin.z + dims[2]} {}
 
 bool Frustum::intersects(const Sphere sphere) const {
     const XMVECTOR sphereCenter    =  sphere.center();
@@ -30,7 +40,7 @@ bool Frustum::intersects(const Sphere sphere) const {
     } else {
         // Test whether the object is in front of the camera.
         // Our projection matrix is reversed, so we use the far plane.
-        const XMVECTOR distance = SSE4::XMVector4Dot(farPlane, XMVectorSetW(sphereCenter, 1.f));
+        const XMVECTOR distance = SSE4::XMVector4Dot(farPlane, sphereCenter);
         // Test the distance against the (negated) radius of the bounding sphere.
         if (XMVectorGetIntX(XMVectorLess(distance, negSphereRadius))) {
             return false;
--- Source/Common/Primitives.h
+++ Source/Common/Primitives.h
@@ -9,12 +9,25 @@ public:
     Sphere() = default;
     // Ctor; takes the center and the radius as input.
     explicit Sphere(const DirectX::XMFLOAT3& center, const float radius);
-    // Returns the center of the sphere in the XYZ part; W = radius.
+    // Returns the center of the sphere. The W component is set to 1.
     DirectX::XMVECTOR center() const;
     // Returns the radius of the sphere in every component.
     DirectX::XMVECTOR radius() const;
 private:
-    DirectX::XMVECTOR m_data;
+	DirectX::XMFLOAT4A m_data;
+};
+
+// Axis-aligned bounding box.
+class AABox {
+public:
+    RULE_OF_ZERO(AABox);
+    AABox() = default;
+    // Ctor; takes the minimal and the maximal bounding points as input.
+    explicit AABox(const DirectX::XMFLOAT3& pMin, const DirectX::XMFLOAT3& pMax);
+	// Ctor; takes the minimal bounding point and the dimensions (in X, Y, Z) as input.
+    explicit AABox(const DirectX::XMFLOAT3& pMin, const float (&dims)[3]);
+private:
+    DirectX::XMFLOAT3 m_pMin, m_pMax;
 };
 
 class Frustum {
