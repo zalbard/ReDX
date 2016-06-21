c6d74b09d1a2563142bd12b3a3ca17cb77eb3192
Evgenii Golubev <zalbard@gmail.com>
2016-06-21 11:36:28 +0200

    Add CloseHandle() failure check

--- Source/D3D12/HelperStructs.hpp
+++ Source/D3D12/HelperStructs.hpp
@@ -340,7 +340,10 @@ namespace D3D12 {
         CHECK_CALL(m_commandQueue->Signal(m_fence.Get(), UINT64_MAX),
                    "Failed to insert a fence into the command queue.");
         syncThread(UINT64_MAX);
-        CloseHandle(m_syncEvent);
+        if (CloseHandle(m_syncEvent) == 0) {
+            printError("Failed to close the synchronization event handle."); 
+            TERMINATE();
+        }
         for (size_t i = 0; i < L; ++i) {
             // Command lists have to be released before the associated
             // root signatures and pipeline state objects.
